d520d36f6ba33f1896435cdcea3c6c8ef49e3ff6
ChloeMVan <74740805+ChloeMVan@users.noreply.github.com>
2025-12-07 22:47:26 -0600

    limit

--- eval/teacache/experiments/read_custom.py
+++ eval/teacache/experiments/read_custom.py
@@ -1,10 +1,15 @@
 import os
 
 def read_lines_to_list(filepath):
+    n = 1
+    i = 0
     lines = []
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     prompt_path = os.path.join(BASE_DIR, filepath)
     with open(prompt_path, "r") as f:
         for line in f:
             lines.append(line.rstrip("\n"))  # keep entire line, remove newline only
+            if i == n:
+                break
+            i+=1
     return lines
